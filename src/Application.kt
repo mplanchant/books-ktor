58afb53b4a298c483b001122c20a9e2f7ed8a9f2
Matthew Planchant <matt@logiccache.co.uk>
2018-11-30 12:11:25 +0000

    Initial commit.

---
+++ src/Application.kt
@@ -0,0 +1,35 @@
+package uk.co.logiccache
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/json/jackson") {
+            data class Book(val title: String, val author: String)
+            val nineteenEightyFour = Book("1984", "George Orwell")
+            call.respond(nineteenEightyFour)
+        }
+    }
+}
+
